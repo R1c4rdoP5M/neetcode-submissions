0c35090a92b888afce630f01f82911d23656e188
Ricardo Matveiev <ricardopr450@gmail.com>
2026-07-25 01:46:52 +0200

    Add: top-k-elements-in-list - submission-10

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-10.py
@@ -0,0 +1,30 @@
+from typing import List
+from collections import Counter
+
+class Solution:
+    def getTop(self, counters: dict):
+        max = 1
+        for key, value in counters.items():
+            if max < value:
+                max = value
+        for key, value in counters.items():
+            if value == max:
+                return key
+    
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        counters = {}
+        for i in range(len(nums)):
+            if nums[i] not in counters:
+                counters[nums[i]] = 1
+            
+            for j in range(len(nums)):
+                if i < j and nums[i] == nums[j]:
+                    counters[nums[i]] += 1
+                    break
+
+        tops = []
+        for i in range(k):
+            tops.append(self.getTop(counters))
+            counters.pop(self.getTop(counters))
+            
+        return tops
